7aada2879afdf26acf4eb64ab7756df75f81505b
xdk78 <xdk78@interia.pl>
2018-03-29 14:59:54 +0200

    Update dependecies

--- src/renderer/components/Anime.vue
+++ src/renderer/components/Anime.vue
@@ -51,7 +51,7 @@
           <v-flex xs12>
 
             <v-expansion-panel popout focusable>
-              <v-expansion-panel-content v-for="(item, key) in anime" :key="key">
+              <v-expansion-panel-content v-for="(item, key) in anime.list" :key="key">
                 <div slot="header" @click="animeEpData(item.number)" >
 
                   <v-list-tile avatar>
@@ -68,15 +68,15 @@
 
                 </div>
 
-                <v-tabs  show-arrows>
+                <v-tabs show-arrows>
                   <v-tabs-slider></v-tabs-slider>
-                  <v-tab v-for="i in 10" :key="i" >
-                    Item {{ i }}
+                  <v-tab v-for="(ep, key) in animeEp" :key="key" >
+                    {{ ep.host }}
                   </v-tab>
                   <v-tabs-items>
-                    <v-tab-item v-for="i in 10" :key="i" >
+                    <v-tab-item v-for="(iep, ikey) in animeEp" :key="ikey" >
                       <v-card flat>
-                        <v-card-text> Item {{ i }}</v-card-text>
+                        <v-card-text> {{ iep.player }}</v-card-text>
                       </v-card>
                     </v-tab-item>
                   </v-tabs-items>
@@ -111,107 +111,105 @@
 </template>
 
 <script>
-  import mal from 'mal-scraper'
+import mal from 'mal-scraper'
 import api from '../api'
-import {
-    mapGetters
-  } from 'vuex'
+import { mapGetters } from 'vuex'
 export default {
-    name: 'Anime',
-    computed: {
-      ...mapGetters({
-        query: 'getQuery'
-      })
-    },
-    data () {
-      return {
-        mal: {
-          title: '',
-          url: '',
-          synopsis: '',
-          picture: '',
-          aired: '?',
-          episodes: '?',
-          score: '?',
-          type: '?',
-          genres: [],
-          members: '',
-          ranked: ''
-        },
-        anime: [],
-        msg: 'Empty :(',
-        error: false,
-        loaded: false,
-        animeEp: []
-      }
-    },
-    methods: {
-      async animeInfo () {
-        try {
-          console.log(this.query)
-          const response = await mal.getInfoFromName(this.query)
-          const data = response
-          console.log(data)
-
-          this.mal.title = data.title
-          this.mal.url = data.url
-          this.mal.picture = data.picture
-          this.mal.synopsis = data.synopsis
-          this.mal.aired = data.aired
-          this.mal.episodes = data.episodes
-          this.mal.score = data.score
-          this.mal.type = data.type
-          this.mal.genres = data.genres
-          this.mal.members = data.members
-          this.mal.ranked = data.ranked
-          this.error = false
-          this.loaded = true
-        } catch (err) {
-          console.log(err)
-          this.error = true
-        }
+  name: 'Anime',
+  computed: {
+    ...mapGetters({
+      query: 'getQuery'
+    })
+  },
+  data () {
+    return {
+      mal: {
+        title: '',
+        url: '',
+        synopsis: '',
+        picture: '',
+        aired: '?',
+        episodes: '?',
+        score: '?',
+        type: '?',
+        genres: [],
+        members: '',
+        ranked: ''
       },
-      async animeData () {
-        try {
-          console.log(this.query)
-          const response = await api.get(`v1/anime/${this.query}`)
-          const data = response.data
-          console.log(data)
-          this.anime = data
-
-          this.error = false
-        } catch (err) {
-          console.log(err)
-          this.error = true
-        }
-      },
-      async animeEpData (n) {
-        console.log(`${this.query}/${n}`)
-        try {
-          console.log(this.query)
-          const response = await api.get(`v1/anime/${this.query}/${n}`)
-          const data = response.data
-          console.log(data)
-          this.animeEp = data
-
-          this.error = false
-        } catch (err) {
-          console.log(err)
-          this.error = true
-        }
-      }
-    },
-    watch: {
-      query: {
-        handler: function (newQuery, oldQuery) {
-          console.log(`oldQuery: ${oldQuery} newQuery: ${newQuery}`)
-          this.animeInfo()
-          this.animeData()
-        }
-      },
-      deep: true
+      anime: [],
+      msg: 'Empty :(',
+      error: false,
+      loaded: false,
+      animeEp: []
     }
+  },
+  methods: {
+    async animeInfo () {
+      try {
+        console.log(this.query)
+        const response = await mal.getInfoFromName(this.query)
+        const data = response
+        console.log(data)
+
+        this.mal.title = data.title
+        this.mal.url = data.url
+        this.mal.picture = data.picture
+        this.mal.synopsis = data.synopsis
+        this.mal.aired = data.aired
+        this.mal.episodes = data.episodes
+        this.mal.score = data.score
+        this.mal.type = data.type
+        this.mal.genres = data.genres
+        this.mal.members = data.members
+        this.mal.ranked = data.ranked
+        this.error = false
+        this.loaded = true
+      } catch (err) {
+        console.log(err)
+        this.error = true
+      }
+    },
+    async animeData () {
+      try {
+        console.log(this.query)
+        const response = await api.get(`v1/anime/${this.query}`)
+        const data = response.data
+        console.log(data)
+        this.anime = data
+
+        this.error = false
+      } catch (err) {
+        console.log(err)
+        this.error = true
+      }
+    },
+    async animeEpData (n) {
+      console.log(`${this.query}/${n}`)
+      try {
+        console.log(this.query)
+        const response = await api.get(`v1/anime/${this.query}/${n}`)
+        const data = response.data
+        console.log(data)
+        this.animeEp = data
+
+        this.error = false
+      } catch (err) {
+        console.log(err)
+        this.error = true
+      }
+    }
+  },
+  watch: {
+    query: {
+      handler: function (newQuery, oldQuery) {
+        console.log(`oldQuery: ${oldQuery} newQuery: ${newQuery}`)
+        this.animeInfo()
+        this.animeData()
+      }
+    },
+    deep: true
   }
+}
 </script>
 
 <style>
